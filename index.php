c4ae9ca5aeda2a8820a985ba5282be8949fa1fcc
Busch <busch@datenschleuder.com>
2013-08-26 02:51:12 +0200

    Removed piwik tracking code

--- index.php
+++ index.php
@@ -5,22 +5,6 @@
 
 <!-- Javascript ================================================== -->
 
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
-
-  (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.datenschleuder.com/";
-    _paq.push(["setTrackerUrl", u+"piwik.php"]);
-    _paq.push(["setSiteId", "1"]);
-    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Piwik Code -->
-
 <!-- Browserweiche -->
 <script type="text/javascript">
 if((navigator.userAgent.match(/iPhone/i)) || 
