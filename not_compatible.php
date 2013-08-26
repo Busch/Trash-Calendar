4d1d7f99df1dfc32eac9fef082d530371659d712
Busch <busch@datenschleuder.com>
2013-08-26 02:23:04 +0200

    Added QR-Code

--- not_compatible.php
+++ not_compatible.php
@@ -37,10 +37,14 @@
         <p>Der Erinnerungsservice funktioniert (bisher) leider nur für iPod, iPhone und iPad.</p>
         <p>Windows Phone wird vermutlich ab 01.01.2014 unterstützt. Android ist leider nicht kompatibel.</p>
       <br>
+      <h3>QR-Code</h3>
+      <p><img src="qrcode.png" alt="QR-Code" title="" /></p>
+      <br>
       <h3>Info</h3>
         <p>Dies ist ein Erinnerungsservice für die GOA Müllabfuhr im Ostalbkreis. Einfach den Bezirk und die Strasse auswählen, um die Abfuhrtermine in den Kalender zu importieren.</p>
         <p>Die Kalenderdaten beinhalten die Hausmüllabfuhr, Gelber Sack, Bioabfall, Blaue Tonne, Grünabfuhr sowie die Altpapiersammlung der Vereine.</p>
         <p>Aktuell sind alle Daten bis einschließlich 31.03.2014 enthalten - die Daten für die darauf folgenden Jahre werden automatisch mit eingepflegt, sobald diese von der GOA zur Verfügung gestellt werden. Eine Erinnerung erfolgt immer am vorherigen Tag um 18:00 Uhr.</p>
+
     </div>
   </div>
 </section>
